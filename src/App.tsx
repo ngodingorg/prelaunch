86bcd05601238f2bb833bc3ca1000639949478b2
Hisam Fahri <me@hisamafahri.com>
2022-05-18 09:50:28 +0700

    feat(background): add background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Component, createSignal } from "solid-js";
-import textLogo from "../src/assets/svg/TextLogo.svg";
-import iconLogo from "../src/assets/svg/IconLogo.svg";
-import menu from "../src/assets/svg/Menu.svg"
+import TextLogo from "../src/assets/svg/TextLogo.svg";
+import IconLogo from "../src/assets/svg/IconLogo.svg";
+import Menu from "../src/assets/svg/Menu.svg";
+import BackgroundIllustration from "../src/assets/image/bg.png";
 
 const App: Component = () => {
   return (
@@ -14,21 +15,31 @@ const App: Component = () => {
 
 export default App;
 
+const Background: Component = () => {
+  return (
+    <div class="right-0 md:bottom-0 mt-8 absolute">
+      <img src={BackgroundIllustration} alt="background" class="" />
+    </div>
+  );
+};
+
 const Header: Component = () => {
-  const [isMenuVisible, setIsMenuVisible] = createSignal(false)
+  const [isMenuVisible, setIsMenuVisible] = createSignal(false);
 
   return (
-    <div class="px-8 lg:px-16 pt-8 flex flex-row justify-between items-center">
+    <div class="px-8 lg:px-16 pt-8 flex flex-row justify-between items-cente">
       <a href="/" class="min-w-fit">
-        <img src={textLogo} alt="logo" class="hidden lg:flex" />
-        <img src={iconLogo} alt="logo" class="flex lg:hidden" />
+        <img src={TextLogo} alt="logo" class="hidden lg:flex" />
+        <img src={IconLogo} alt="logo" class="flex lg:hidden" />
       </a>
       <div>
-        <div onClick={() => {
-          setIsMenuVisible(!isMenuVisible())
-          console.log(isMenuVisible())
-        }}>
-          <img src={menu} alt="menu" class="flex lg:hidden cursor-pointer" />
+        <div
+          onClick={() => {
+            setIsMenuVisible(!isMenuVisible());
+            console.log(isMenuVisible());
+          }}
+        >
+          <img src={Menu} alt="menu" class="flex lg:hidden cursor-pointer" />
         </div>
         <div class="hidden lg:grid grid-cols-3 gap-x-6 text-right text-white font-ng-mono items-center">
           <a
@@ -59,19 +70,22 @@ const Header: Component = () => {
 
 const Body: Component = () => {
   return (
-    <div class="mx-8 lg:mx-16 mt-8 md:mt-16 grid grid-rows-2 lg:grid-cols-2 gap-y-8 md:gap-y-16 lg:gap-x-24">
-      <div>
-        <p class="font-ng-mono text-white text-4xl md:text-6xl">
-          Kini semua orang bisa belajar &lt;
-          <span class="text-ng-yellow">pemrograman_</span>&gt;
-        </p>
+    <>
+      <div class="mx-8 lg:mx-16 mt-8 md:mt-16 grid grid-rows-2 lg:grid-cols-2 gap-y-8 md:gap-y-16 max-h-fit lg:gap-x-24">
+        <div>
+          <p class="font-ng-mono text-white text-4xl md:text-6xl">
+            Kini semua orang bisa belajar &lt;
+            <span class="text-ng-yellow">pemrograman_</span>&gt;
+          </p>
+        </div>
+        <div>
+          <p class="font-ng-text text-white text-lg md:text-2xl font-light">
+            Misi &lt;<span class="text-ng-yellow">ngoding.org</span>&gt; adalah
+            untuk membantu banyak orang untuk belajar pemrograman secara gratis.
+          </p>
+        </div>
       </div>
-      <div>
-        <p class="font-ng-text text-white text-lg md:text-2xl font-light">
-          Misi &lt;<span class="text-ng-yellow">ngoding.org</span>&gt; adalah
-          untuk membantu banyak orang untuk belajar pemrograman secara gratis.
-        </p>
-      </div>
-    </div>
+      <Background />
+    </>
   );
 };
